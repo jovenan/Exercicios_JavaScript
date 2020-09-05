6f24a9650d0170e2951391b4b25ae2e709461150
Jonatas <Jonatas.venancio@outlook.com>
2020-09-05 15:37:58 -0300

    Exercicios com estrutura condicional

---
+++ Aula_4/Ex006.js
@@ -0,0 +1,7 @@
+var vel = 61
+
+console.log(`A velocidade que passou é ${vel}`)
+if (vel > 60) {
+    console.log(`Voce ultrapassou a velocidade!!!`)
+}
+console.log("Dirija sempre com cinto de segurança!")
---
+++ Aula_4/Ex010.js
@@ -0,0 +1,9 @@
+var idade = 17
+if (idade < 16) {
+    console.log("Nao vota")
+} else if (idade < 18 || idade > 60) {
+    console.log("Opicional")
+} else {
+    console.log("Voto obrigatorio")
+}
+
