2fafee9969c121256217ffb0d7f6fbd7d1ce70d6
Jonatas Venancio <49959786+jovenan@users.noreply.github.com>
2021-07-10 10:02:48 -0300

    Delete multiplica.test.js

--- JS Intermediario/multiplica.test.js
+++
@@ -1,7 +0,0 @@
-const multiplica = require('./multiplica');
-
-test('Multiplica valor de 2 * 4 e resulta em 8', () => {
-    expect(multiplica(2, 4)).toBe(8);
-    expect(multiplica(2, 4)).not.toBeNull();
-    expect(multiplica(2, 4)).not.toBeUndefined();
-});
